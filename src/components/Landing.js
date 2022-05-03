0405048b847757f009013ab0b0d23080f58eb283
xomastyle- <homenich101@gmail.com>
2022-05-03 17:37:45 +0300

    add navigation to router

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -19,7 +19,8 @@ class Landing extends React.Component {
     }
 
     goToRestaurant = () => {
-        console.log('go to restaurant!')
+        const {url} = this.state;
+        this.props.history.push(`/restaurant/${url}`);
     }
 
     render(){
