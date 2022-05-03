9642b10fbf052ac7df0fe77bb375d6467bc47fa4
xomastyle- <homenich101@gmail.com>
2022-05-03 16:51:06 +0300

    Add app file, create new components

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,30 +2,57 @@ import React from "react";
 import restaurants from "../sample-restaurants";
 
 class Landing extends React.Component {
+    state = {
+        display: false,
+        title: "",
+        url: ""
+    };
+
+    displayList = () => {
+        const {display} = this.state;
+        this.setState({display: !display});
+    }
+
+    getTitle = restaurant => {
+        const {title, url} = restaurant;
+        this.setState({title, url, display: false})
+    }
+
+    goToRestaurant = () => {
+        console.log('go to restaurant!')
+    }
+
     render(){
         return (
                 <div className="restaurant_select">
                 <div className="restaurant_select_top">
-                    <div className="restaurant_select_top-header, font-effect-outline">
-                        Choise Restourant!
+                    <div className="restaurant_select_top-cursour">
+                    <div
+                    onClick={this.displayList} 
+                    className="restaurant_select_top-header, font-effect-outline">
+                        {this.state.title ? this.state.title : "Choise Restourant!"}
+                    </div>
                     </div>
                     <div className="arrow_picker">
                         <div className="arrow_picker-up"></div>
                         <div className="arrow_picker-down"></div>
                     </div>
                 </div>
+                {this.state.display?(
                 <div className="restaurant_select_bottom">
                     <ul>
                         {restaurants.map(restaurant=>{
-                                    return <li key={restaurant.id}>
-                                        {restaurant.title}
+                                    return <li 
+                                    onClick={ () => this.getTitle(restaurant)} 
+                                    key={restaurant.id}>{restaurant.title}
                                     </li>
                                 })
                         }
                     </ul>
                 </div>
+                ) :null}
 
-                <button>sign in restorant</button>
+                {this.state.title && !this.state.display ? <button onClick={this.goToRestaurant}>go to restaurant</button> : null}
             </div>
         )
     }
