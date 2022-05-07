ac9f2f645608be0a844f31a30d8376ebaed4613e
xomastyle- <homenich101@gmail.com>
2022-05-07 22:17:39 +0300

    use hook useState in component

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
 
     static propTypes = {
         match: PropTypes.object
-    }
+    };
 
     state = {
         burgers: {},
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,68 +1,64 @@
-import React from "react";
+import React, {useState} from "react";
 import restaurants from "../sample-restaurants";
 import PropTypes from 'prop-types';
 
-class Landing extends React.Component {
+const Landing = (props) => {
 
-    static propTypes = {
-        history: PropTypes.object
-    }
+    const [display, toggleDisplay] = useState(false);
+    const [title, setTitle] = useState('');
+    const [url, setUrl] = useState('');
 
-    state = {
-        display: false,
-        title: "",
-        url: ""
+    const displayList = () => {
+        toggleDisplay(!display);
     };
 
-    displayList = () => {
-        const {display} = this.state;
-        this.setState({display: !display});
-    }
-
-    getTitle = restaurant => {
+    const getTitle = restaurant => {
         const {title, url} = restaurant;
-        this.setState({title, url, display: false})
-    }
+        toggleDisplay(false);
+        setTitle(title);
+        setUrl(url);
+    };
 
-    goToRestaurant = () => {
-        const {url} = this.state;
-        this.props.history.push(`/restaurant/${url}`);
-    }
+    const goToRestaurant = () => {
+        props.history.push(`/restaurant/${url}`);
+    };
 
-    render(){
-        return (
-                <div className="restaurant_select">
-                <div className="restaurant_select_top">
-                    <div className="restaurant_select_top-cursour">
-                    <div
-                    onClick={this.displayList} 
-                    className="restaurant_select_top-header, font-effect-outline">
-                        {this.state.title ? this.state.title : "Choise Restourant!"}
-                    </div>
-                    </div>
-                    <div className="arrow_picker">
-                        <div className="arrow_picker-up"></div>
-                        <div className="arrow_picker-down"></div>
-                    </div>
-                </div>
-                {this.state.display?(
-                <div className="restaurant_select_bottom">
-                    <ul>
-                        {restaurants.map(restaurant=>{
-                                    return <li 
-                                    onClick={ () => this.getTitle(restaurant)} 
-                                    key={restaurant.id}>{restaurant.title}
-                                    </li>
-                                })
-                        }
-                    </ul>
-                </div>
-                ) :null}
-
-                {this.state.title && !this.state.display ? <button onClick={this.goToRestaurant}>go to restaurant</button> : null}
+    return (
+        <div className="restaurant_select">
+        <div className="restaurant_select_top">
+            <div className="restaurant_select_top-cursour">
+            <div
+            onClick={displayList} 
+            className="restaurant_select_top-header, font-effect-outline">
+                {title ? title : "Choise Restourant!"}
             </div>
-        )
-    }
+            </div>
+            <div className="arrow_picker">
+                <div className="arrow_picker-up"></div>
+                <div className="arrow_picker-down"></div>
+            </div>
+        </div>
+        {display?(
+        <div className="restaurant_select_bottom">
+            <ul>
+                {restaurants.map(restaurant=>{
+                            return <li 
+                            onClick={ () => getTitle(restaurant)} 
+                            key={restaurant.id}>{restaurant.title}
+                            </li>
+                        })
+                }
+            </ul>
+        </div>
+        ) :null}
+
+        {title && !display ? <button onClick={goToRestaurant}>go to restaurant</button> : null}
+    </div>
+)
+}
+
+Landing.propTypes = {
+    history: PropTypes.object
 }
 
 export default Landing;
